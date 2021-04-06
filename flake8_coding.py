1106d953dde998ab6dd7f951efc804dea562d609
Nikita Shulga <nshulga@fb.com>
2021-04-06 10:08:00 -0700

    Add `optional-ascii-coding` option
    Do not force 'coding:' option on pure-ASCII source files

--- flake8_coding.py
+++ flake8_coding.py
@@ -2,7 +2,7 @@
 
 import re
 
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 
 
 class CodingChecker(object):
@@ -22,10 +22,15 @@ class CodingChecker(object):
             '--no-accept-encodings', action='store_true', parse_from_config=True,
             help="Warn for files containing a `coding:` magic comment"
         )
+        parser.add_option(
+            '--optional-ascii-coding', action='store_true', parse_from_config=True,
+            help="Do not force 'coding:' header on ascii only files"
+        )
 
         if hasattr(parser, 'config_options'):  # for flake8 < 3.0
             parser.config_options.append('accept-encodings')
             parser.config_options.append('no-accept-encodings')
+            parser.config_options.append('optional-ascii-coding')
 
     @classmethod
     def parse_options(cls, options):
@@ -33,31 +38,36 @@ class CodingChecker(object):
             cls.encodings = None
         else:
             cls.encodings = [e.strip().lower() for e in options.accept_encodings.split(',')]
+        cls.optional_ascii_coding = options.optional_ascii_coding
 
-    def read_headers(self):
+    @classmethod
+    def has_non_ascii_characters(cls, lines):
+        return any(any(ord(c) > 127 for c in line) for line in lines)
+
+    def read_lines(self):
         if self.filename in ('stdin', '-', None):
             try:
                 # flake8 >= v3.0
                 from flake8.engine import pep8 as stdin_utils
             except ImportError:
                 from flake8 import utils as stdin_utils
-            return stdin_utils.stdin_get_value().splitlines(True)[:2]
+            return stdin_utils.stdin_get_value().splitlines(True)
         else:
             try:
                 import pycodestyle
             except ImportError:
                 import pep8 as pycodestyle
-            return pycodestyle.readlines(self.filename)[:2]
+            return pycodestyle.readlines(self.filename)
 
     def run(self):
         try:
             # PEP-263 says: a magic comment must be placed into the source
             #               files either as first or second line in the file
-            lines = self.read_headers()
+            lines = self.read_lines()
             if len(lines) == 0:
                 return
 
-            for lineno, line in enumerate(lines, start=1):
+            for lineno, line in enumerate(lines[:2], start=1):
                 matched = re.search(r'coding[:=]\s*([-\w.]+)', line, re.IGNORECASE)
                 if matched:
                     if self.encodings:
@@ -68,6 +78,7 @@ class CodingChecker(object):
                     break
             else:
                 if self.encodings:
-                    yield 1, 0, "C101 Coding magic comment not found", type(self)
+                    if not self.optional_ascii_coding or self.has_non_ascii_characters(lines):
+                        yield 1, 0, "C101 Coding magic comment not found", type(self)
         except IOError:
             pass
